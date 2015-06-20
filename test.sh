24573bc7b2bbed7334a6e77469a7fe3706e9407e
Andrew Martin <sublimino@gmail.com>
2015-06-20 19:32:52 +0100

    Include docker-compose-local.yml instead of rewriting docker-compose.yml

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 PAZ_IP=${DOCKER_HOST:-$(ip route get 1 | awk '{print $NF;exit}')}
 
 echo PAZ_IP $PAZ_IP
-sed -E "s/(ETCD_ADVERTISE_CLIENT_URLS)=.*/\1=http:\/\/${PAZ_IP}:2379/g" -i.bak docker-compose.yml
+sed -E "s/(ETCD_ADVERTISE_CLIENT_URLS)=.*/\1=http:\/\/${PAZ_IP}:2379/g" -i docker-compose-local.yml
 
 docker-compose pull ; docker-compose build && docker-compose up -d && sleep 3 && lab && docker-compose stop
