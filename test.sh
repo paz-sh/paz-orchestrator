269e4f157131f3c961ffae6914ccb4c956b63c59
Andrew Martin <sublimino@gmail.com>
2015-06-21 22:16:53 +0100

    OSX compatibility
    TODO: write out docker-compose-local.yml

--- test.sh
+++ test.sh
@@ -3,6 +3,13 @@
 PAZ_IP=${DOCKER_HOST:-$(ip route get 1 | awk '{print $NF;exit}')}
 
 echo PAZ_IP $PAZ_IP
-sed -E "s/(ETCD_ADVERTISE_CLIENT_URLS)=.*/\1=http:\/\/${PAZ_IP}:2379/g" -i docker-compose-local.yml
+if [[ -z ${DOCKER_HOST} ]]; then
+  PAZ_IP=$(ip route get 1 | awk '{print $NF;exit}')
+  sed -E "s/(ETCD_ADVERTISE_CLIENT_URLS)=.*/\1=http:\/\/${PAZ_IP}:2379/g" -i docker-compose-local.yml
+else
+  # DOCKER_HOST set so assume OSX
+  PAZ_IP=$(echo $DOCKER_HOST | cut -d: -f2 | cut -d/ -f3)
+  sed -e "s/(ETCD_ADVERTISE_CLIENT_URLS)=.*/\1=http:\/\/${PAZ_IP}:2379/g" -i'' docker-compose-local.yml
+fi
 
 docker-compose pull ; docker-compose build && docker-compose up -d && sleep 3 && lab && docker-compose stop
